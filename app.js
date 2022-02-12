691da35939b5ebcf9037baac9f70710e70eff2c8
shamsmikdad <shams.mikdad@gmail.com>
2022-02-12 23:29:05 +0200

    start-up

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require ("express")
+const app = express()
+const mongoose = require ("mongoose")
+const path = require ("path")
+// const connectDB = require('./connect');
+
+app.use(express.json());
+// const service = require ("./routes/api/services")
+
+//static path
+// app.use(express.static(path.join(__dirname,'public',)))
+
+//config  DB
+const db = require("./db/URI").mongoURI
+//connect db
+
+const port = process.env.PORT || 3000;
+
+//app.listen(port,()=>console.log(`listen on port ${port}`))
+
+const start = async () => {
+    try {
+      await mongoose
+        .connect(db)
+        .then(()=>{console.log("connect to db ")})
+        .catch(err => {console.log(err)});
+      app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  start();
+  
---
+++ db/URI.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb+srv://shms:011011@express.n8ce3.mongodb.net/SERVIC_DB?retryWrites=true&w=majority'
+}
